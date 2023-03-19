3d4b6699eb14ff395152bcf100cf16030fb6b032
adelindascalu <adelinc.dascalu@gmail.com>
2023-03-19 16:21:42 +0200

    Small updates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,11 +12,12 @@ export default function About() {
           <h2 className='heading-tertiary'>About me</h2>
           <h3 className='heading-secondary'>Who I am</h3>
           <p className='about-description'>
-            Hello! My name is Adelin Dascălu and I enjoy building things from
-            scratch. <br /> It all started back in 2022 when I joined a Web
-            Development training program and realized it was a perfect fit. My
-            goal is to improve my skills, with the main focus on designing and
-            building websites that offer a smooth user experience.
+            Hello! My name is Adelin Dascălu and I am a Junior Front-End
+            Developer based in Bucharest, Romania. It all started back in 2022
+            when I joined a Web Development training program and realized it was
+            a perfect fit. My goal is to improve my skills, with the main focus
+            on designing and building websites that offer a smooth user
+            experience.
           </p>
           <a className='btn-dark resume-btn' href={CV} download>
             <FontAwesomeIcon icon={faDownload} />
