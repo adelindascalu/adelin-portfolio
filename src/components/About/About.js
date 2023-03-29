44ccd7cfc83e178f3cb41ab1a488d0d8e9d0c851
adelindascalu <adelinc.dascalu@gmail.com>
2023-03-29 10:22:01 +0300

    CV update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import "./About.css";
 import portfolioPhoto from "../../assets/images/WhatsApp Image 2023-01-28 at 14.14.03 cut.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
-import CV from "../../assets/docs/Adelin_Dascalu_CV_EN.pdf";
+import CV from "../../assets/docs/Adelin_Dascalu_CV_EN_.pdf";
 
 export default function About() {
   return (
@@ -14,9 +14,9 @@ export default function About() {
           <p className='about-description'>
             Hello! My name is Adelin Dascălu and I am a Junior Front-End
             Developer based in Bucharest, Romania. It all started back in 2022
-            when I joined a Web Development training program and realized it was
-            a perfect fit. My goal is to improve my skills, with the main focus
-            on designing and building websites that offer a smooth user
+            when I joined a Web Development training program and I realized it
+            was a perfect fit. My goal is to improve my skills, with the main
+            focus on designing and building websites that offer a smooth user
             experience.
           </p>
           <a className='btn-dark resume-btn' href={CV} download>
